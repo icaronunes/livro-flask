6b7d0b6ce0adce3731bef02452cce3075e4c591b
Icaro Nunes <icaro.nunes@linx.com.br>
2022-12-05 20:54:45 -0300

    primeira parte

---
+++ app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+from config import app_config, app_active
+from flask_sqlalchemy import SQLAlchemy
+
+config = app_config[app_active]
+
+
+def create_app(config_name):
+    app = Flask(__name__, template_folder='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(config.APP)
+    db.init_app(app)
+    app.secret_key = config.SECRET
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    @app.route('/')
+    def index():
+        return 'hello World'
+
+    return app
--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os;
-import random
-import string
+import random, string
 
 
 class Config(object):
     CSRF_ENABLED = True
-    SECRET = ""
+    SECRET ='ysb_92=qe#djf8%ng+a*#4rt#5%3*4k5%i2bck*gn@w3@f&-&'
     TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)))
     APP = None
+    SQLALCHEMY_DATEBASE_URI = 'mysql+mysqldb://user:passwd@host:port/database'
 
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(Config):
     TESTING = True
     DEBUG = True
     IP_HOST = 'localhost'
     PORT_HOST = '8000'
-    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
+    URL_MAIN = 'http://%s:%s/' % (IP_HOST, PORT_HOST)
 
 
 class TestingConfig(Config):
@@ -23,14 +23,14 @@ class TestingConfig(Config):
     DEBUG = True
     IP_HOST = 'localhost'
     PORT_HOST = '8000'
-    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
+    URL_MAIN = 'http://%s:%s/' % (IP_HOST, PORT_HOST)
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
     IP_HOST = 'localhost'
     PORT_HOST = '8000'
-    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
+    URL_MAIN = 'http://%s:%s/' % (IP_HOST, PORT_HOST)
 
 app_config = {
     'development': DevelopmentConfig(),
@@ -38,4 +38,4 @@ app_config = {
     'production': ProductionConfig()
 }
 
-app_active = os.getenv('FLASK_ENV')
+app_active = os.getenv('FLASK_ENV', 'development')
