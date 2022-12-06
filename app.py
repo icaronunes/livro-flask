1b2d5255b704c30e6743e1044a6adc38901a2aff
Icaro <icarornunes@gmail.com>
2022-12-05 22:41:16 -0300

    criado Models

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-
 from config import app_config, app_active
 from flask_sqlalchemy import SQLAlchemy
 
 config = app_config[app_active]
-
+db = SQLAlchemy(config.APP)
 
 def create_app(config_name):
-    app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    app = Flask(__name__, template_folder='templates') 
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATEBASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = SQLAlchemy(config.APP)
+
     db.init_app(app)
     app.secret_key = config.SECRET
     app.config.from_object(app_config[config_name])
